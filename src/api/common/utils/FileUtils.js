21bcf1f180004d052829eb76af69f92039f10d77
John Morris <johnmorris@tuta.io>
2020-09-04 16:25:49 +0200

    replace invalid character in file names when exporting emails via zip file, #2160

---
+++ src/api/common/utils/FileUtils.js
@@ -0,0 +1,17 @@
+/**
+ * removes invalid characters from the given filename
+ * by replacing them with underscores (non-platform-specific)
+ */
+export function sanitizeFilename(filename: string): string {
+	// / ? < > \ : * | "
+	const illegalRe = /[\/\?<>\\:\*\|"]/g
+	// unicode control codes
+	const controlRe = /[\x00-\x1f\x80-\x9f]/g
+	// trailing period in windows file names
+	const windowsTrailingRe = /[\. ]+$/
+
+	return filename
+		.replace(illegalRe, "_")
+		.replace(controlRe, "_")
+		.replace(windowsTrailingRe, "_")
+}
